6c2d40383bd2b485def6698cbdbbf8dd0fde834f
michasacuer <michasacuer3@gmail.com>
2020-01-09 10:49:45 +0100

    Added exception filter, registered MediatR

---
+++ src/Thinning.Web/Filters/CustomExceptionFilterAttribute.cs
@@ -0,0 +1,18 @@
+﻿namespace Thinning.Web.Filters
+{
+    using System;
+    using System.Net;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class CustomExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            context.HttpContext.Response.ContentType = "application/json";
+            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Result = new JsonResult(new ExceptionMessageViewModel(context));
+        }
+    }
+}
--- src/Thinning.Web/Startup.cs
+++ src/Thinning.Web/Startup.cs
@@ -1,14 +1,18 @@
 namespace Thinning.Web
 {
+    using System.Reflection;
     using Autofac;
+    using MediatR;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using Thinning.Application.Test.Command.AddTest;
     using Thinning.Persistence;
     using Thinning.Persistence.Interfaces;
+    using Thinning.Web.Filters;
 
     public class Startup
     {
@@ -26,11 +30,18 @@ namespace Thinning.Web
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers(options =>
+                options.Filters.Add(typeof(CustomExceptionFilterAttribute)))
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddScoped<IThinningDbContext, ThinningDbContext>();
             services.AddDbContext<ThinningDbContext>(options =>
                 options.UseSqlServer(_configuration.GetConnectionString("ThinningDatabase")));
+            services.AddMediatR(typeof(AddTestCommandHandler).GetTypeInfo().Assembly);
         }
 
         public void ConfigureContainer(ContainerBuilder builder)
