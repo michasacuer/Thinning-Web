438779850e0e7a4371c84b79d172cad4ae565132
michasacuer <michasacuer3@gmail.com>
2020-01-11 20:27:32 +0100

    Added BaseController and mock for TestController

---
+++ src/Thinning.Web/Api/BaseController.cs
@@ -0,0 +1,15 @@
+﻿namespace Thinning.Web.Api
+{
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.DependencyInjection;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ?? (_mediator = HttpContext.RequestServices.GetService<IMediator>());
+    }
+}
---
+++ src/Thinning.Web/Api/TestController.cs
@@ -0,0 +1,15 @@
+﻿namespace Thinning.Web.Api
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Thinning.Application.Test.Command.AddTest;
+
+    public class TestController : BaseController
+    {
+        [HttpPost("add")]
+        public async Task<IActionResult> AddTest(AddTestCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
